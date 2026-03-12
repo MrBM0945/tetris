451c771983b5a7d65ea7be57f1c02748c80bda39
Bohdan Myskiv <bogdammyskiv321@gmail.com>
2026-03-12 20:04:41 +0200

    Add full board logic

--- board.py
+++ board.py
@@ -15,13 +15,64 @@ class Board:
         return grid
 
     def update_grid(self):
-        pass
+        """Оновлює ігрову сітку відповідно до поточних заблокованих позицій."""
+        self.grid = self.create_grid()
 
     def validate_space(self, piece):
-        pass
+        """
+        Перевіряє, чи знаходиться фігура в межах поля і чи не накладається на інші блоки.
+        Припускаємо, що piece має метод get_formatted_shape(), який повертає [(x, y), (x, y), ...]
+        """
+        # Створюємо список усіх вільних позицій на полі
+        accepted_positions = [[(j, i) for j in range(self.columns) if self.grid[i][j] == (0, 0, 0)] for i in range(self.rows)]
+        accepted_positions = [pos for sublist in accepted_positions for pos in sublist] # Згладжуємо список
+
+        # Отримуємо координати блоків поточної фігури
+        # (Замініть get_formatted_shape() на метод, який використовується у вашому класі Piece)
+        formatted = piece.get_formatted_shape()
+
+        for pos in formatted:
+            if pos not in accepted_positions:
+                # Дозволяємо фігурі знаходитися вище верхньої межі екрану (y < 0) при її появі
+                if pos[1] > -1:
+                    return False
+        return True
 
     def clear_rows(self):
-        pass
+        """
+        Аналог clear_rows(grid, locked) з main.py.
+        """
+        inc = 0
+        ind = -1  # Додано, щоб уникнути попередження лінтера, про яке ви питали раніше
+
+        for i in range(self.rows - 1, -1, -1):
+            row = self.grid[i]
+            if (0, 0, 0) not in row:
+                inc += 1
+                ind = i
+                for j in range(self.columns):
+                    try:
+                        del self.locked_positions[(j, i)]
+                    except KeyError:
+                        continue
+                        
+        if inc > 0:
+            for key in sorted(list(self.locked_positions), key=lambda x: x[1])[::-1]:
+                x, y = key
+                if y < ind:
+                    newKey = (x, y + inc)
+                    self.locked_positions[newKey] = self.locked_positions.pop(key)
+            
+            # Після зсуву обов'язково оновлюємо сітку
+            self.update_grid()
+            
+        return inc  # Повертаємо кількість видалених рядків для підрахунку очок
 
     def check_game_over(self):
-        pass
+        """
+        Перевіряє, чи не досягли заблоковані блоки верхньої межі ігрового поля.
+        """
+        for (x, y) in self.locked_positions:
+            if y < 1:  # Якщо будь-який блок знаходиться на першому рядку або вище
+                return True
+        return False
